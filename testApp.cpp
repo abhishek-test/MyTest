7677515f6dd66661f4fef0929961ca40a02b306f
Srivastava <abhishekx.srivastava@intel.com>
2017-11-24 22:03:41 +0530

    added video reading code

--- testApp.cpp
+++ testApp.cpp
@@ -1,7 +1,24 @@
-#include <Opencv2/imgproc/imgproc.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+
+using namespace std;
 using namespace cv;
 
 int main()
 {
-	int a =0 ;
+	VideoCapture vid(0);
+	if(!vid.isOpened())
+	  { cout << " !! Error reading camera " << endl; return -1; }
+
+	Mat inputFrame;
+
+	while(1)
+	{
+		vid >> inputFrame;
+		laneTracking(inputFrame);
+		if(waitKey(30) == 27)
+			break;
+	}
 }
